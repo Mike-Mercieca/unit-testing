5b480dc3833a06d676076a6339a645bf78a8bd19
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-07-12 10:48:34 +0100

    Group test together

--- test/getLargest.test.js
+++ test/getLargest.test.js
@@ -1,17 +1,15 @@
-const getLargest = require("./getLargest");
-
-test("getLargest is a function", () => {
-    expect(typeof getLargest).toBe("function");
-})
+const getLargest = require("../getLargest");
 
 
-
-
-
-
-
-test("getLargest returns the largest value", () => {
-    const result = getLargest(1, 2, 3);
-
-    expect(result).toEqual(3);
-})
+describe("getLargest", () => {
+    
+    test("getLargest is a function", () => {
+        expect(typeof getLargest).toBe("function");
+    })
+    
+    test("getLargest returns the largest value", () => {
+        const result = getLargest(1, 2, 3);
+    
+        expect(result).toEqual(3);
+    })
+});
